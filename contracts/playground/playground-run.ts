4ab9d6fdcc7f68b8f548ebf36b7c24ffff8cbbaf
Erickrs2 <erickrs2esuarez@gmail.com>
2024-02-29 17:24:58 -0300

    all

--- contracts/playground/playground-run.ts
+++ contracts/playground/playground-run.ts
@@ -1,13 +1,34 @@
-import { Lucid, Blockfrost } from "https://deno.land/x/lucid/mod.ts"
+// export PROJECTID=xxxxxx
+// export SEEDPHRASE="your seed phrase"
+// deno run -A ./contracts/playground/playground-run.ts
+
+import { Lucid, Blockfrost } from "https://deno.land/x/lucid@0.10.7/mod.ts";
+import { mkLucidWallet } from "npm:@marlowe.io/wallet";
+import { mkRuntimeLifecycle } from "npm:@marlowe.io/runtime-lifecycle";
+import { mkRestClient } from "npm:@marlowe.io/runtime-rest-client";
+
+const runtimeURL =
+  "https://marlowe-runtime-preprod-web.demo.scdev.aws.iohkdev.io";
 
 const projectId = Deno.env.get("PROJECTID");
-console.log(projectId)
+const seedPhrase = Deno.env.get("SEEDPHRASE");
+console.log(projectId, seedPhrase)
 
-/** 
 const lucid = await Lucid.new(
     new Blockfrost("https://cardano-preprod.blockfrost.io/api/v0", projectId),
     "Preprod"
   );
-
+lucid.selectWalletFromSeed(seedPhrase!);
 console.log(lucid)
-*/
+
+const wallet = mkLucidWallet(lucid);
+console.log(wallet)
+
+const lifecycle = mkRuntimeLifecycle({
+    runtimeURL,
+    wallet,
+  });
+console.log(lifecycle)
+
+const restAPI = mkRestClient(runtimeURL);
+console.log(restAPI)
